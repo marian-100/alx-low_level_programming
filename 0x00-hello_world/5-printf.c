aaa4291372966000a439cef84b208f99fbd298ae
marian-100 <marianakanyare7@gmail.com>
2023-04-23 13:43:33 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - A C program with a printf function
- * Return: 0
+ * Return:
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n"
-	return (0);
+	return (0);,
 }
