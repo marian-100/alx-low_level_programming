a01bb3df82e5deb66d6e4507b7858a70238ce34d
marian-100 <marianakanyare7@gmail.com>
2023-04-24 17:27:07 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: 'A program that prints alphabets lowercase and uppercase'
+  *Return: Always 0
+  */
+int main(void)
+{
+	char c;	
+	char d;
+
+	while 
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while 
+		(d <= 'z') {
+			putchar(d);
+			d++;
+		}	
+	putchar('\n');
+	return (0);
+}
